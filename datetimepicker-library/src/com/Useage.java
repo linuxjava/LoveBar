f065be53de158d154a1b52156e0bd233800a6375
linuxjava <516282426@qq.com>
2015-12-25 19:26:27 +0800

    添加日期选择库

---
+++ datetimepicker-library/src/com/Useage.java
@@ -0,0 +1,16 @@
+package com;
+
+import com.fourmob.datetimepicker.date.DatePickerDialog;
+
+/**
+ * Created by xiaoguochang on 2015/12/25.
+ */
+public class Useage {
+    public static void show(){
+//        DatePickerDialog.newInstance(new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePickerDialog datePickerDialog, int year, int month, int day) {
+//            }
+//        }, 2015, 12, 20).show(null, "datePicker");
+    }
+}
